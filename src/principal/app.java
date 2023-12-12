ef1494a884037b7f877cc759a2ce2e42f9fa5816
Diurno <Diurno@C06PC06.informatica.comercio>
2023-12-12 16:53:11 +0100

    actualizar precio reserva

--- src/principal/app.java
+++ src/principal/app.java
@@ -1,6 +1,7 @@
 package principal;
 
 import controlador.Controller;
+import interfaz.ActualizarDatos;
 import interfaz.CrearReserva;
 import interfaz.MostrarReservas;
 import modelo.AccesoDAO;
@@ -10,9 +11,10 @@ public class app {
     public static void main(String[] args) {
         MostrarReservas vista = new MostrarReservas();
         CrearReserva viewReserva = new CrearReserva();
+        ActualizarDatos viewActualizar = new ActualizarDatos();
         AccesoDAO modelo = new AccesoDAO();
         
-        Controller controlador = new Controller(vista,viewReserva, modelo);
+        Controller controlador = new Controller(vista,viewReserva,viewActualizar, modelo);
         
     }
 }
